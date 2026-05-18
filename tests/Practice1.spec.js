0e60b7b365a10a9ec9480c75ab5a5fdff35dd320
Prathisha Kappalli <prathishakappalli@gmail.com>
2026-05-19 03:44:34 +0530

    quick change

--- tests/Practice1.spec.js
+++ tests/Practice1.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+//made first change in the code
 
 test("@web test", async ({ page }) => {
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
